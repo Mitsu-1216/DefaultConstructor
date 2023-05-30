2b464b349ef3f9a2aeb9b6fe85912602fab79ea8
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-05-30 19:42:08 +0900

    プロジェクト ファイルを追加します。

---
+++ defaultConstructor/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace defaultConstructor
+{
+    internal class Animal
+    {
+        int id;
+        string name;
+
+        public Animal(string name, int id = 0)
+        {
+            this.id = id;
+            this.name = name;
+
+            if (id==0)
+            {
+                Console.WriteLine(name + "さん、"+"はじめまして！");
+            }
+            else
+            {
+                Console.WriteLine( name + "さん、"+"いつもありがとうございます！" );
+            }
+
+        }
+
+
+
+    }
+}
---
+++ defaultConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace defaultConstructor
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
+        {
+            Animal r = new Animal("うさぎ");
+            Animal t = new Animal("かめ",10);
+        }
+    }
+}
